9641cec06d9d3e439fbf4f7ca5930bb757f49c5f
Schmelzberry <jelsberry96@gmail.com>
2023-09-01 11:36:09 -0700

    pass RED phase - test#3 earth.js

--- __tests__/earth.test.js
+++ __tests__/earth.test.js
@@ -14,6 +14,12 @@ describe('earthAge', () => {
     expect(exampleAge.mercury).toEqual(exampleAge.age * .24);
   });
 
+  test('should correctly return age multiplied by .62 stored in its own key', () => {
+    let number = Number;
+    const exampleAge = new Age(number);
+    expect(exampleAge.venus).toEqual(exampleAge.age * .62);
+  });
+
 
 });
 
--- src/earth.js
+++ src/earth.js
@@ -2,6 +2,7 @@ export default class EarthAge {
  constructor(age) {
   this.age=age;
   this.mercury= this.age * .24;
+  
   }
 
 } 
