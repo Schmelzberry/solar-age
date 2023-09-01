093859c7e17fc3eac1acc9ff594967170800dbcb
Schmelzberry <jelsberry96@gmail.com>
2023-09-01 11:10:02 -0700

    REFAC - pass green phase test#1 earth.js

--- __tests__/earth.test.js
+++ __tests__/earth.test.js
@@ -3,7 +3,10 @@ import Age from './../src/earth.js';
 describe('earthAge', () => {
 
   test('should return a number', () => {
-    const exampleAge = new Age(6);
-    expect(exampleAge.age).toEqual(6);
+    let number = Number;
+    const exampleAge = new Age(number);
+    expect(exampleAge.age).toEqual(number);
   });
+
+
 });
