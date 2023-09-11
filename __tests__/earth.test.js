64649bce44f7f98da19325cdf0634183d34bc83d
Schmelzberry <jelsberry96@gmail.com>
2023-09-10 20:29:50 -0700

    refactor tests, beforeEach loop WIP

--- __tests__/earth.test.js
+++ __tests__/earth.test.js
@@ -3,64 +3,35 @@ import Age from './../src/earth.js';
 describe('earthAge', () => {
 
   test('should return a number', () => {
-    let number = 1;
-    const exampleAge = new Age(number);
+    const exampleAge = new Age(1);
     expect(exampleAge.age).toEqual(1);
   });
 
   test('should correctly return age multiplied by .24, stored in mercury key of constructor', () => {
-    let number = 1;
-    const exampleAge = new Age(number);
+    const exampleAge = new Age(1);
     exampleAge.mercury();
     expect(exampleAge.mercuryAge).toEqual(.24);
   });
 
   test('should correctly return age multiplied by .62 stored in venus key of constructor', () => {
-    let number = 1;
-    const exampleAge = new Age(number);
+    const exampleAge = new Age(1);
     exampleAge.venus();
     expect(exampleAge.venusAge).toEqual(.62);
   });
 
   test('should correctly return age multiplied by 1.88 stored in mars key of constructor', () => {
-    let number = 1;
-    const exampleAge = new Age(number);
+    const exampleAge = new Age(1);
     exampleAge.mars();
     expect(exampleAge.marsAge).toEqual(1.88);
   });
 
   test('should correctly return age multiplied by 11.86 stored in its own key', () => {
-    let number = 1;
-    const exampleAge = new Age(number);
+    const exampleAge = new Age(1);
     exampleAge.jupiter();
     expect(exampleAge.jupiterAge).toEqual(11.86);
   });
 
-  test('should determine how many years have passed on each planet since a past birthday', () => {
-    let currentAge = 50; 
-    let pastDate = 10;
-    let currentMinusTargetAge = currentAge - pastDate;
-    const exampleAge = new Age(currentAge);
-    const newAges = exampleAge.yearsPassed(pastDate);
-    const pastMercuryAge = currentMinusTargetAge * 0.24;
-    const pastVenusAge = currentMinusTargetAge * 0.62;
-    const pastMarsAge = currentMinusTargetAge * 1.88;
-    const pastJupiterAge = currentMinusTargetAge * 11.86;
-    expect(newAges).toEqual([currentMinusTargetAge, pastMercuryAge, pastVenusAge, pastMarsAge, pastJupiterAge]);
-  });
 
-  test('should determine how many years have passed on each planet given a future birthday', () => {
-    let currentAge = 50; 
-    let futureDate = 55;
-    let futureMinusTargetAge = futureDate - currentAge;
-    const exampleAge = new Age(currentAge);
-    const newAges = exampleAge.yearsUntil(futureDate);
-    const futureMercuryAge = futureMinusTargetAge * 0.24;
-    const futureVenusAge = futureMinusTargetAge * 0.62;
-    const futureMarsAge = futureMinusTargetAge * 1.88;
-    const futureJupiterAge = futureMinusTargetAge * 11.86;
-    expect(newAges).toEqual([futureMinusTargetAge, futureMercuryAge, futureVenusAge, futureMarsAge, futureJupiterAge]);
-  });
 
 
 });
--- src/earth.js
+++ src/earth.js
@@ -28,23 +28,24 @@ export default class Age {
    }
 
 
-  yearsPassed(pastDate) {
-    pastDate = Number(pastDate);
-    let ageMinusDate = (this.age - pastDate);
-    this.mercury = ageMinusDate * .24;
-    this.venus = ageMinusDate * .62;
-    this.mars = ageMinusDate * 1.88;
-    this.jupiter = ageMinusDate *  11.86;
-    return [ageMinusDate, this.mercury, this.venus, this.mars, this.jupiter];
+//   yearsPassed(pastDate) {
+//     pastDate = Number(pastDate);
+//     let ageMinusDate = (this.age - pastDate);
+//     this.mercury = ageMinusDate * .24;
+//     this.venus = ageMinusDate * .62;
+//     this.mars = ageMinusDate * 1.88;
+//     this.jupiter = ageMinusDate *  11.86;
+//     return [ageMinusDate, this.mercury, this.venus, this.mars, this.jupiter];
 
+//   }
+//   yearsUntil(futureDate) {
+//     futureDate = Number(futureDate);
+//     let howLong = (futureDate - this.age);
+//     this.mercury = howLong * .24;
+//     this.venus = howLong * .62;
+//     this.mars = howLong * 1.88;
+//     this.jupiter = howLong *  11.86;
+//     return [howLong, this.mercury, this.venus, this.mars, this.jupiter];
+//   }
+// } 
   }
-  yearsUntil(futureDate) {
-    futureDate = Number(futureDate);
-    let howLong = (futureDate - this.age);
-    this.mercury = howLong * .24;
-    this.venus = howLong * .62;
-    this.mars = howLong * 1.88;
-    this.jupiter = howLong *  11.86;
-    return [howLong, this.mercury, this.venus, this.mars, this.jupiter];
-  }
-} 
